69de7dd8c6e8551cbf03304c3b89936cc7ddf27b
JohannesH <johe7425@student.uu.se>
2013-03-26 02:56:57 +0100

    Added cryptbreaker class, does nothing

---
+++ src/vigenere/CryptBreaker.java
@@ -0,0 +1,13 @@
+package vigenere;
+/**
+ * 1. Get cipher
+ * 2. ???
+ * 3. PROFIT!(and the original message)
+ * @author JohannesH
+ *
+ */
+public class CryptBreaker {
+	public String breakCipher(String message){
+		return "";
+	}
+}
